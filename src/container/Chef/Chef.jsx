496c587f7378c1d26eace6c37da645443511a8cc
Muhammad Taham <taha_shahid@outlook.com>
2022-06-19 20:56:03 +0500

    chef section

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import {images} from '../../constants';
+import {SubHeading} from '../../components'
+
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding" >
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef" />  
+    </div>
   </div>
 );
 
